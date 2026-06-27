aa2a360f699bbcf2f6b920beb164242431c99fe3
ridhamu <ridhauwu@gmail.com>
2026-06-27 12:34:29 +0700

    done chap. 4.4, next chap. 4.5

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,16 +1,58 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 
-	quotedJsonValue := strconv.Quote(jsonValue)
+	quotedJSONValue := strconv.Quote(jsonValue)
 
-	return []byte(quotedJsonValue), nil
+	return []byte(quotedJSONValue), nil
 }
+
+// UnmarshalJSON Implement a UnmarshalJSON() method on the Runtime type so that it satisfies the
+// json.Unmarshaler interface. IMPORTANT: Because UnmarshalJSON() needs to modify the
+// receiver (our Runtime type), we must use a pointer receiver for this to work
+// correctly. Otherwise, we will only be modifying a copy (which is then discarded when
+// this method returns).
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONvalue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONvalue, " ")
+
+	// sanity check
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// parse into int32
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
+}
+
+// Unmarshaler is the interface implemented by types
+// that can unmarshal a JSON description of themselves.
+// The input can be assumed to be a valid encoding of
+// a JSON value. UnmarshalJSON must copy the JSON data
+// if it wishes to retain the data after returning.
+// type Unmarshaler interface {
+// 	UnmarshalJSON([]byte) error
+// }
