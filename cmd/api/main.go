2651d3bc1799af6716b7ac6b3fe6170dd615c717
ridhamu <ridhauwu@gmail.com>
2026-06-28 21:53:11 +0700

    done chap 7.1, next chap 7.2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	_ "github.com/lib/pq" // To register the driver.
+	"github.com/ridhamu/greenlight/internal/data"
 )
 
 const version = "1.0.0"
@@ -29,6 +30,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -59,6 +61,7 @@ func main() {
 	app := application{
 		config: config,
 		logger: logger,
+		models: data.NewModels(dbPool),
 	}
 
 	srv := http.Server{
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/ridhamu/greenlight/internal/validator"
@@ -15,6 +16,10 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+type MovieModel struct {
+	DB *sql.DB
 }
 
 func ValidateMovie(v *validator.Validator, movie *Movie) {
@@ -32,4 +37,20 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
