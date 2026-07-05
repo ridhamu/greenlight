427598bc9954164d154c1c88fa6c467fde1b36cd
ridhamu <ridhauwu@gmail.com>
2026-07-05 22:51:04 +0700

    done adding configurable rate limiter

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -49,28 +49,29 @@ func (app *application) rateLimiter(next http.Handler) http.Handler {
 	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip, _, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil {
-			app.serverErrorResponse(w, r, err)
-			return
-		}
-
-		mu.Lock()
-		if _, ok := clients[ip]; !ok {
-			clients[ip] = &client{
-				limiter: rate.NewLimiter(2, 4),
+		if app.config.limiter.enabled {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				app.serverErrorResponse(w, r, err)
+				return
 			}
-		}
 
-		clients[ip].lastseen = time.Now()
+			mu.Lock()
+			if _, ok := clients[ip]; !ok {
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+				}
+			}
 
-		if !clients[ip].limiter.Allow() {
+			clients[ip].lastseen = time.Now()
+
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
 			mu.Unlock()
-			app.rateLimitExceededResponse(w, r)
-			return
 		}
-		mu.Unlock()
-
 		next.ServeHTTP(w, r)
 	})
 }
